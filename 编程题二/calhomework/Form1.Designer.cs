9c7154ff4c776c57da1f367ad0084b45c824c539
Li haomin <2430249817@qq.com>
2018-08-16 18:37:55 +0800

    1

---
+++ 编程题二/calhomework/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace calhomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "正方形";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "长方形";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "三角形";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "圆";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(343, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(343, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(249, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "请输入";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(68, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 144);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "请选择图形类型";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(249, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "请输入";
+            this.label3.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "cm",
+            "inch"});
+            this.comboBox1.Location = new System.Drawing.Point(449, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(78, 20);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(234, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "计算";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 362);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "通用面积计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 编程题二/calhomework/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calhomework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar) && e.KeyChar !='.') 
+　　        {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 8 && !Char.IsDigit(e.KeyChar) && e.KeyChar !='.') 
+　　        {
+                e.Handled = true;
+            }
+        }
+        public float number_1, numeber_2;
+        public bool squ, rec, tri, cir;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = textBox1.Visible = comboBox1.Visible = true;
+            label3.Visible = textBox2.Visible = false;
+            label1.Text = "请输入边长";
+            squ = true;
+            rec = tri = cir = false;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "请输入长";
+            label3.Text = "请输入宽";
+            label1.Visible = textBox1.Visible = comboBox1.Visible = label3.Visible = textBox2.Visible = true;
+            rec = true;
+            squ = tri = cir = false;
+        }
+
+       
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "请输入高";
+            label3.Text = "请输入底";
+            label1.Visible = textBox1.Visible = comboBox1.Visible = label3.Visible = textBox2.Visible = true;
+            tri = true;
+            squ = rec = cir = false;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "请输入直径";
+            label1.Visible = textBox1.Visible = comboBox1.Visible = true;
+            label3.Visible = textBox2.Visible = false;
+            cir = true;
+            squ = rec = tri = false;
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            if(squ)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("请输入边长");
+                }
+                else if(comboBox1.Text=="")
+                {
+                    MessageBox.Show("请选择单位");
+                }
+                else
+                {
+                    double size = double.Parse(textBox1.Text.ToString().Trim());
+                    double area = size * size;
+                    if (this.comboBox1.Text == "cm")
+                    {
+                        MessageBox.Show("正方形  边长为"+ Math.Round(size,3) + "厘米  面积为"+ Math.Round(area, 3) + "平方厘米");
+                    }
+                    else if (this.comboBox1.Text == "inch")
+                    {
+                        size *= 2.54;
+                        area = area * 2.54 * 2.54;
+                        MessageBox.Show("正方形  边长为"+ Math.Round(size, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                }
+            }
+            if (rec)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("请输入长");
+                }
+                else if(textBox2.Text=="")
+                {
+                    MessageBox.Show("请输入宽");
+                }
+                else if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("请选择单位");
+                }
+                else
+                {
+                    double size1 = double.Parse(textBox1.Text.ToString().Trim());
+                    double size2 = double.Parse(textBox2.Text.ToString().Trim());
+                    double area = size1 * size2;
+                    Math.Round(area, 3);
+                    if (this.comboBox1.Text == "cm")
+                    {
+                        MessageBox.Show("长方形  长为" + Math.Round(size1, 3) + "厘米  宽为"+ Math.Round(size2, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                    else if (this.comboBox1.Text == "inch")
+                    {
+                        size1 *= 2.54;
+                        size2 *= 2.54;
+                        area = area * 2.54 * 2.54;
+                        MessageBox.Show("长方形  长为" + Math.Round(size1, 3) + "厘米  宽为" + Math.Round(size2, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                }
+            }
+            if (cir)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("请输入直径");
+                }
+                else if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("请选择单位");
+                }
+                else
+                {
+                    double dia = double.Parse(textBox1.Text.ToString().Trim());
+                    double area = dia * dia * 3.14 / 4;
+                    if (this.comboBox1.Text == "cm")
+                    {
+                        MessageBox.Show("圆形  直径为" + Math.Round(dia, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                    else if (this.comboBox1.Text == "inch")
+                    {
+                        dia *= 2.54;
+                        area = area * 2.54 * 2.54;
+                        MessageBox.Show("圆形  直径为" + Math.Round(dia, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                }
+            }
+            if (tri)
+            {
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("请输入高");
+                }
+                else if (textBox2.Text == "")
+                {
+                    MessageBox.Show("请输入底");
+                }
+                else if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("请选择单位");
+                }
+                else
+                {
+                    double high = double.Parse(textBox1.Text.ToString().Trim());
+                    double bottom = double.Parse(textBox2.Text.ToString().Trim());
+                    double area = high * bottom / 2;
+                    if (this.comboBox1.Text == "cm")
+                    {
+                        MessageBox.Show("三角形  高为" + Math.Round(high, 3) + "厘米  底为" + Math.Round(bottom, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                    else if (this.comboBox1.Text == "inch")
+                    {
+                        high *= 2.54;
+                        bottom *= 2.54;
+                        area = area * 2.54 * 2.54;
+                        MessageBox.Show("三角形  高为" + Math.Round(high, 3) + "厘米  底为" + Math.Round(bottom, 3) + "厘米  面积为" + Math.Round(area, 3) + "平方厘米");
+                    }
+                }
+            }
+        }
+          private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+          private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    } 
+}
